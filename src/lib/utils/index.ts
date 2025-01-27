481a0cad257f49fb11b6bfbc397ff94566952d1e
Jason Coawette <jason.coawette@gmail.com>
2025-01-26 21:58:11 -0700

    starting clean

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,6 +2,10 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 
+// ------------------------------------------- //
+// CLASSNAME EDITOR [========================] ::
+// ------------------------------------------- //
 export function cn(...inputs: (string | undefined | null | false)[]) {
   return twMerge(clsx(inputs));
 }
+// ------------------------------------------- //
