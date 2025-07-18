cb050ea293a9d6b0526a53cb0656e41bd27ec9a6
JasonCoawette <jason.coawette@gmail.com>
2025-07-18 06:52:45 -0700

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+
+	export const prerender = true;
+export const trailingSlash = 'always'; 
+</script>
+
+{@render children()}
---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+export default {
+  kit: {
+		adapter: adapter({ fallback: '404.html' })
+  },
+  preprocess: vitePreprocess()
+};
