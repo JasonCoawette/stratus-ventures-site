9be74378fdd09da7cfc1f0b452dbe0ab43143ff2
JasonCoawette <jason.coawette@gmail.com>
2025-07-19 08:59:23 -0700

    Github Pages Automation v1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	
+	export const prerender = true;
+	export const trailingSlash = 'always';
+
 	let { children } = $props();
 
-	export const prerender = true;
-export const trailingSlash = 'always'; 
 </script>
 
 {@render children()}
